06ffc3d44df6dcdfa5ac39f36eff35037a63aff1
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-08 17:09:54 +0600

    modify the Model Class for show the title in list

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,7 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.title
+    
