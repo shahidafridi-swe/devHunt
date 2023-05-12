1eced8dc7546b552bb23530346efe1676018f86e
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-13 04:16:12 +0600

    projects and project html template has done

--- devHunt/settings.py
+++ devHunt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zz@dr)*o_-uxq0io!n-hn3rx9pnzpwoo)$yhjbbc9=k-=9zj$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost' , '127.0.0.1']
 
