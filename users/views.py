0e23f60dce5ed1a6fc025d216c6912ee0b09013f
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-15 02:37:18 +0600

    profile page done

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Profile
 
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {
         'profiles': profiles
     }
     return render(request, 'users/profiles.html', context)
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topskills = profile.skill_set.exclude(description__exact="")
+    otherskills = profile.skill_set.filter(description="")
+
+    context = {
+        'profile': profile,
+        'topskills':topskills,
+        'otherskills':otherskills
+    }
+    return render(request, 'users/profile.html', context)
