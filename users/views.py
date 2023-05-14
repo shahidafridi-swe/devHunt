a1b7f79f4310773615a323896e9ce2845b8093c5
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-15 00:02:04 +0600

    profiles page done

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
 def profiles(request):
+    profiles = Profile.objects.all()
     context = {
-
+        'profiles': profiles
     }
     return render(request, 'users/profiles.html', context)
