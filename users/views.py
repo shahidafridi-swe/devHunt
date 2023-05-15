12f7f1b17220820a4a025437306c078ba03f1dfe
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-15 19:43:14 +0600

    login required

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from .models import Profile
