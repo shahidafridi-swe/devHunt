69a5a3a577c6daf7ce121860aa2779f12d0f5740
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-16 20:28:21 +0600

    registration form modify

--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from .forms import CustomUserCreationForm
 from .models import Profile
 
 
@@ -41,10 +40,10 @@ def logoutUser(request):
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
