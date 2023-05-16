639085d26e246db24250e420ef6b5137804108ab
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-16 21:10:19 +0600

    register page done

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,14 @@ from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User 
+        model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
         labels = {
-            'first_name':'Full Name'
+            'first_name': 'Full Name',
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
