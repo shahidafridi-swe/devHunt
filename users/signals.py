530fc7358d930ecb723d73ca7f8a4f8e68c2cf16
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-21 16:34:07 +0600

    welcome email send

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
 
 
@@ -13,6 +15,15 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name
+        )
+        subject = 'Welcome to devHunt'
+        body = 'We are glad you are here!!!'
+        send_mail(
+            subject,
+            body,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently = False,
         )
 
 
