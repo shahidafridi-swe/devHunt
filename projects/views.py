4ab1f8854c04fb16172098e1b2c572ed0daf2009
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-22 01:20:14 +0600

    add tag by update project form and create projecy form

--- projects/views.py
+++ projects/views.py
@@ -35,7 +35,7 @@ def project(request, pk):
 
     context = {
         'project': project,
-        'form':form
+        'form': form
     }
     return render(request, 'projects/single-project.html', context)
 
@@ -45,12 +45,17 @@ def createProject(request):
     profile = request.user.profile
 
     form = ProjectForm()
+    
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
+        newtags = request.POST.get('newtags').replace(',', ' ').split()
         if form.is_valid:
             project = form.save(commit=False)
             project.owner = profile
             project.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             return redirect('account')
 
     context = {'form': form}
@@ -63,9 +68,13 @@ def updateProject(request, pk):
     project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', ' ').split()
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid:
-            form.save()
+            project = form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             return redirect('account')
     context = {
         'form': form
