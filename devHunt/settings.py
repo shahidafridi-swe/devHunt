7f2a09a9940cd216875aac2c82832024a95618eb
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-22 23:59:07 +0600

    ready to deploy in railway

--- devHunt/settings.py
+++ devHunt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zz@dr)*o_-uxq0io!n-hn3rx9pnzpwoo)$yhjbbc9=k-=9zj$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
